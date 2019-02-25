406503d1af65ffcf34224a4b1494f3f2b9a9402f
Magnus Conrad Hyll <magnus@hyll.no>
2019-02-25 20:48:57 +0100

    Singleton logger, renamed to get_logger

--- logger.py
+++ logger.py
@@ -2,21 +2,25 @@ import logging
 import logging.handlers
 import sys
 
+_logger = None
 
-def setup_logger(*, log_level='INFO', syslog=True, stdout=False):
-    log = logging.getLogger('captiveportal')
-    log.setLevel(logging.getLevelName(log_level))
 
-    if syslog:
-        syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
-        formatter = logging.Formatter('%(name)s: [%(levelname)s] %(message)s')
-        syslog_handler.setFormatter(formatter)
-        log.addHandler(syslog_handler)
+def get_logger(*, log_level='INFO', syslog=True, stdout=False):
+    global _logger
+    if not _logger:
+        _logger = logging.getLogger('captiveportal')
+        _logger.setLevel(logging.getLevelName(log_level))
 
-    if stdout:
-        stdout_hander = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter('%(asctime)s %(name)s: [%(levelname)s] %(message)s')
-        stdout_hander.setFormatter(formatter)
-        log.addHandler(stdout_hander)
+        if syslog:
+            syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+            formatter = logging.Formatter('%(name)s: [%(levelname)s] %(message)s')
+            syslog_handler.setFormatter(formatter)
+            _logger.addHandler(syslog_handler)
 
-    return log
+        if stdout:
+            stdout_hander = logging.StreamHandler(sys.stdout)
+            formatter = logging.Formatter('%(asctime)s %(name)s: [%(levelname)s] %(message)s')
+            stdout_hander.setFormatter(formatter)
+            _logger.addHandler(stdout_hander)
+
+    return _logger
