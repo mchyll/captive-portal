2918dc097b1714617f93951d9b30ad5484dd83c2
Magnus Conrad Hyll <magnus@hyll.no>
2019-02-25 22:40:43 +0100

    Logging in Flask app, fixed HTTP stuff
    - Added logging in CaptivePortal.py flask app
    - Changed to return 403 Forbidden when unauthorized user tries to ban by
    POSTing to /admin
    - Removed GET method from /login endpoint as it causes an error
    - Show the user an error if iptables fails when trying to allow the user
    internet access
    - Added get_client_ip function to check for X-Real-IP before falling
    back to REMOTE_ADDR
    - Added requirements.txt file

--- CaptivePortal.py
+++ CaptivePortal.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request, flash, redirect
-import sqlite3
+from flask import Flask, render_template, request, flash, redirect, abort
 import os
 import ipa_auth as ipa
 import db
 import ip_management
+import logger
 
 app = Flask(__name__)
+
+# TODO: change this to defaults (syslog=True, stdout=False) in prod
+log = logger.get_logger(syslog=False, stdout=True, log_level='DEBUG')
+
+
+def get_client_ip():
+    try:
+        return request.headers['X-Real-IP']
+    except KeyError:
+        ip = request.environ['REMOTE_ADDR']
+        log.debug('No X-Real-IP header, using REMOTE_ADDR: {} (not behind nginx proxy?)'.format(ip))
+        return ip
 
 
 def ban_user(user):
@@ -22,9 +34,10 @@ def ban_user(user):
 @app.route('/admin', methods=["GET"])
 def admin_page():
 
-    ip = request.headers['X-Real-IP']
+    ip = get_client_ip()
 
     if not db.isAdmin(ip):
+        log.warning('Client {} tried to access admin page but is not drifter'.format(ip))
         return redirect('/')
 
     data = db.getUserList()
@@ -35,46 +48,54 @@ def admin_page():
 @app.route('/admin', methods=["POST"])
 def ban_request():
     user = request.form['user']
-
-    ip = request.headers['X-Real-IP']
+    ip = get_client_ip()
 
     if not db.isAdmin(ip):
-        return redirect('/')
+        log.warning('Client {} tried to ban user {} but is not drifter'.format(ip, user))
+        return abort(status=403)
 
     if ban_user(user):
-        flash('Ban successful.','success')
+        log.info('Banned user {}'.format(user))
+        flash('Ban successful.', 'success')
         return redirect('/admin')
     else:
+        log.error('Banning of user {} failed'.format(user))
         flash('Ban failed.', 'danger')
         return redirect('/admin')
 
 
-
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=["POST"])
 def login_page():
 
     username = request.form['username']
     password = request.form['password']
 
     if db.isBanned(username):
+        log.warning('Banned user {} tried to log in'.format(username))
         flash('User is banned.', 'danger')
         return redirect('/')
 
     if ipa.valid_login(username, password):
         admin_flag = ipa.isAdmin(username, password)
 
-        ip = request.environ['REMOTE_ADDR']
+        ip = get_client_ip()
         if not db.setUser(ip, username, admin_flag):
+            log.error('Database error when trying to add user to clients table (user {}, ip {})'.format(username, ip))
             flash('Database bind failed. Contact Drift.', 'danger')
             return redirect('/')
 
         # Allow the client IP address to access the internet
-        ip_management.iptables_allow_ip(ip)
+        if not ip_management.iptables_allow_ip(ip):
+            log.error('Iptables error when trying to allow client (user {}, ip {})'.format(username, ip))
+            flash('An error occurred when trying to give you internet access. Contact Drift.', 'danger')
+            return redirect('/')
 
         return render_template("home.html")
     else:
+        log.warning('Invalid credentials for user {}'.format(username))
         flash('Invalid username or password.', 'danger')
         return redirect('/')
+
 
 @app.route('/')
 def home():
@@ -82,5 +103,6 @@ def home():
 
 
 if __name__ == "__main__":
+    log.info('Flask application starting')
     app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0')
