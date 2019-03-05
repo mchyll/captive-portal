e733831454e6250547f963628f57c5f6a6e05ab0
Magnus Conrad Hyll <magnus@hyll.no>
2019-03-05 18:34:47 +0100

    Fixed database setup python script

--- setup_db.py
+++ setup_db.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
 import sqlite3
 
 
 qry = open('create_db_tables.sql', 'r').read()
 conn = sqlite3.connect('user-ip.db')
 c = conn.cursor()
-c.execute(qry)
+c.executescript(qry)
 conn.commit()
 c.close()
 conn.close()
+print('Database setup finished')
