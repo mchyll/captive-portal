59f6980c84c3e3be92703f27cffc18e5321c88b7
Magnus Conrad Hyll <magnus@hyll.no>
2019-03-06 03:55:52 +0100

    Added Flask listening port to config.yml

--- CaptivePortal.py
+++ CaptivePortal.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
 
     log.info('Flask application starting')
     app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=config['flask_port'])
